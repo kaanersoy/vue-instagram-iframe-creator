1e0e91c58202287a1cbefcbe0c923e1291cd1874
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-18 22:24:51 +0300

    Added fetch function to /api/auth route get user info

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express = require('express');
-
+const axios = require('axios');
 const app = express();
 require('dotenv').config();
 
-app.use(express.static('public/'));
 //Midd-wares
+app.use(express.static('public/'));
 
 //Routes
 app.get('/api/',(req,res)  => {
     res.send('HI FROM API!');
 })
 
-app.get('/api/auth/', (req,res) => {
-    if(req.body.code){
-        res.redirect('/');
+app.get('/api/auth/', async (req,res) => {
+    if(process.env.FB_USER_AUTH_SECRET){
+        try{
+            const result = await axios.get(`https://graph.instagram.com/me?fields=username&access_token=${process.env.FB_USER_AUTH_SECRET}`);
+            res.json(result.data);
+        }
+        catch(err){
+            console.error(err)
+        }
+        
     }else{
-        res.send(400);
+        res.sendStatus(400);
     }
 })
 
@@ -23,7 +30,7 @@ app.get('/*', (req,res) => {
     res.sendStatus(404);
 })
 
-//Process
+//process section
 const PORT = process.env.PORT || 3000;
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server up and running on ->', `http://localhost:${PORT}`);

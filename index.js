7030bd68a7954c39375d0f63a667cb3caeda57e5
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-18 22:38:04 +0300

    Update to get query

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.get('/api/',(req,res)  => {
 })
 
 app.get('/api/auth/', async (req,res) => {
-    if(req.params.code){
-        res.send(req.query.code);
+    if(req.query("code")){
+        res.send(req.query("code"));
     }
     if(!process.env.FB_USER_AUTH_SECRET){
         res.sendStatus(404);   

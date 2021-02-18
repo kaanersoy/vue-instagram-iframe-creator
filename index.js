f11be374f3a35ab66e39001ed5d385485be645f0
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-18 22:32:28 +0300

    Update for auth route to check code from instagram API

--- index.js
+++ index.js
@@ -12,17 +12,20 @@ app.get('/api/',(req,res)  => {
 })
 
 app.get('/api/auth/', async (req,res) => {
-    if(process.env.FB_USER_AUTH_SECRET){
-        try{
-            const result = await axios.get(`https://graph.instagram.com/me?fields=username&access_token=${process.env.FB_USER_AUTH_SECRET}`);
-            res.json(result.data);
-        }
-        catch(err){
-            console.error(err)
-        }
-        
-    }else{
-        res.sendStatus(400);
+    if(req.params.code){
+        res.send(req.params.code);
+    }
+
+    if(!process.env.FB_USER_AUTH_SECRET){
+        res.sendStatus(404);   
+    }
+    try{
+        const result = await axios.get(`https://graph.instagram.com/me?fields=username&access_token=${process.env.FB_USER_AUTH_SECRET}`);
+        res.json(result.data);
+    }
+    catch(err){
+        console.error(err)
+        res.sendStatus(301);
     }
 })
 

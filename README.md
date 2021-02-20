64c0e413c67f57083bb7a61efd703ee21801d3c1
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-20 12:04:59 +0300

    Update code with formetter

--- README.md
+++ README.md
@@ -1,32 +1,37 @@
 # Instagram IFrame Creator
 
 This app allows to you create IFrames with your Instagram account.
+
 - Just sign in with your instagram and select your style!
 - Get the code and import your HTML template!
 - Voila🎉, your Instagram Iframe is ready to use!
-> (in developement)
+  > (in developement)
+
 ## Install
 
 1. Clone the project with: `$ git clone https://github.com/kaanersoy/vue-instagram-iframe-creator.git`
 2. Run the server like:
+
 ```shell
 //for deploy
-npm run start 
+npm run start
 //for developement
 npm run dev
 ```
+
 > HERE WE GO!🤘
+
 ## Completed Tasks 🤯🤯😎
 
- - [x] Build a Restfull API for secure API key.
- - [x] Deploy in Heroku.
- - [x] Add a HTML page for pretty little cute visualizations.  [(Visit!)](https://whispering-anchorage-68692.herokuapp.com/)
- - [x] Create a `Instagram Basic Display` app and get a secret key.
- - [x] Get a succesfully login with Instagram!.
- - [x] Get user info from Instagram API.
+- [x] Build a Restfull API for secure API key.
+- [x] Deploy in Heroku.
+- [x] Add a HTML page for pretty little cute visualizations. [(Visit!)](https://whispering-anchorage-68692.herokuapp.com/)
+- [x] Create a `Instagram Basic Display` app and get a secret key.
+- [x] Get a succesfully login with Instagram!.
+- [x] Get user info from Instagram API.
 
 ## TODO LIST 📚📚
- 
- - [ ] Get instagram posts informations.
- - [ ] Build a Vue app for front-end.
- - [ ]  ... 
+
+- [ ] Get instagram posts informations.
+- [ ] Build a Vue app for front-end.
+- [ ] ...
--- index.js
+++ index.js
@@ -1,53 +1,59 @@
-const express = require('express');
-const axios = require('axios');
-const helmet = require('helmet');
+const express = require("express");
+const axios = require("axios");
+const helmet = require("helmet");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
 //Midd-wares
-app.use(express.static('public/'));
-app.use(helmet({
-    contentSecurityPolicy: false
-}))
+app.use(express.static("public/"));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 //Routes
-app.get('/api/',(req,res)  => {
-    res.send('HI FROM API!');
-})
+app.get("/api/", (req, res) => {
+  res.send("HI FROM API!");
+});
 
-app.get('/api/auth/', async (req,res) => {
-    const {FB_APP_ID, FB_API_SECRET_KEY, APP_AUTH_URL} = process.env;
-    if(req.query.code){
-        const params = new URLSearchParams();
-        params.append('client_id', FB_APP_ID);
-        params.append('client_secret', FB_API_SECRET_KEY);
-        params.append('grant_type', 'authorization_code');
-        params.append('redirect_uri', APP_AUTH_URL);
-        params.append('code', req.query.code);
-        const config = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }
-        try{    
-            const result = await axios.post('https://api.instagram.com/oauth/access_token/', params, config);
-            res.send({
-                data: result.data,
-                result: 'Result is that!'
-            });
-        }catch(err){
-            res.send(err);
-        }
+app.get("/api/auth/", async (req, res) => {
+  const { FB_APP_ID, FB_API_SECRET_KEY, APP_AUTH_URL } = process.env;
+  if (req.query.code) {
+    const params = new URLSearchParams();
+    params.append("client_id", FB_APP_ID);
+    params.append("client_secret", FB_API_SECRET_KEY);
+    params.append("grant_type", "authorization_code");
+    params.append("redirect_uri", APP_AUTH_URL);
+    params.append("code", req.query.code);
+    const config = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    };
+    try {
+      const result = await axios.post(
+        "https://api.instagram.com/oauth/access_token/",
+        params,
+        config
+      );
+      res.send({
+        data: result.data,
+        result: "Result is that!",
+      });
+    } catch (err) {
+      res.send(err);
     }
-    const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${FB_APP_ID}&redirect_uri=${APP_AUTH_URL}&scope=user_profile,user_media&response_type=code`;
-    res.redirect(authUrl);
-})
+  }
+  const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${FB_APP_ID}&redirect_uri=${APP_AUTH_URL}&scope=user_profile,user_media&response_type=code`;
+  res.redirect(authUrl);
+});
 
-app.get('/*', (req,res) => {
-    res.sendStatus(404);
-})
+app.get("/*", (req, res) => {
+  res.sendStatus(404);
+});
 
 //process section
 const PORT = process.env.PORT || 3000;
 app.listen(process.env.PORT || 3000, () => {
-    console.log('Server up and running on ->', `http://localhost:${PORT}`);
-})
+  console.log("Server up and running on ->", `http://localhost:${PORT}`);
+});
